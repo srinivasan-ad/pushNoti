0202671eb8745f714583ee6c0f1bca368ab76ced
srinivasan-ad <forprogramingpurpose@gmail.com>
2024-08-16 22:32:30 +0530

    push notifications :saprkles:

--- sw.js
+++ sw.js
@@ -1,3 +1,16 @@
-self.addEventListener('push',() => {
-    self.registration.sendNotification('test message' , {});
+self.addEventListener('push',(e) => {
+    let options = {
+        body : "This notification was generated from a push :) !",
+        vibrate : [100,50,100],
+        data : {
+dateOfArrival : Date.now(),
+primaryKey : '2'
+        },
+        actions :[ {
+action : 'explore',
+title : 'Explore this new world !',
+},
+{action:'close',title : 'Close'}]
+    };
+    e.waitUntil(self.registration.sendNotification('Namaste universe' , options));
 });
