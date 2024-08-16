a59936b891e56e8ef387b3bde418a9a891204bc1
srinivasan-ad <forprogramingpurpose@gmail.com>
2024-08-16 23:59:23 +0530

    push notifications :zap:

--- push.js
+++ push.js
@@ -1,14 +1,26 @@
-const push = require('web-push');
+const push = require("web-push");
 let vapidKeys = {
-    
-        publicKey: 'BGaj-2wVNung__pDef8rChvu04JzUxVLPTNUNLDOc9JqLy_5en-s3U8SlydUIK1O18uUVJzFt14N2JBNWJVZ7QI',
-        privateKey: '9_Qo3qMXRAJadB5rhCOwgGlvGZsH_Y1yePy30BL5e8E'
-      
-}
+  publicKey:
+    "BGaj-2wVNung__pDef8rChvu04JzUxVLPTNUNLDOc9JqLy_5en-s3U8SlydUIK1O18uUVJzFt14N2JBNWJVZ7QI",
+  privateKey: "9_Qo3qMXRAJadB5rhCOwgGlvGZsH_Y1yePy30BL5e8E",
+};
 const payload = JSON.stringify({
-    title: "Namaste universe !",
-    body: "I am in :)",
-  });
-push.setVapidDetails('mailto:1mp22cs039.bgscet.ac.in',vapidKeys.publicKey,vapidKeys.privateKey);
-let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/dRbUtFHUvMs:APA91bEKLU_pMb8hWXyTKxdFp8OVdkM5YHdapnuUha_9fl3s26wLM-nAXjc7beoew9ozpc3_cZUGeEuLBs6nUMZYiviqgJRIt5yd5sZQlrpTO6NhsY2RsqjljqV7WGSvtyWb4wyT4WUx","expirationTime":null,"keys":{"p256dh":"BL7W4c43O1hA15_Xs372g21SBXFi1XM2iFfXxw7d4fMVO9keSFGUmojN0zDlRc1MV9h1PU9hPwwQFw3df3T3b3k","auth":"Y5iwpbCkWrjpsW-_ZFCpHg"}}
-push.sendNotification(sub,payload);
+  title: "Namaste universe !",
+  body: "I am in :)",
+});
+push.setVapidDetails(
+  "mailto:1mp22cs039.bgscet.ac.in",
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+);
+let sub = {
+  endpoint:
+    "https://fcm.googleapis.com/fcm/send/f2TAQxx9mvo:APA91bG7Y8lvyS6YBcC5LNFjOvm8hxh0hPULiKVeUnUbL-4DEDOqhAJgWBnYrFrWys10pyFc7E4lhrutweUf6O4H9ZX5pc5u_anak1humuuWoLXzvVQln6J_o3jmmewfW__d_f1TPhiJ",
+  expirationTime: null,
+  keys: {
+    p256dh:
+      "BD8-tYd_RsFjukb8-YM5-uI8DwrUJyAgFFdSSfo45DEEvj4pJzxzp0uo0kT6-fcy5mUCxgNOT1CronGBGvyqcWo",
+    auth: "kNQ9hhJ5PhIVC2g_Vjl3fg",
+  },
+};
+push.sendNotification(sub, payload);
--- sw.js
+++ sw.js
@@ -1,25 +1,28 @@
-self.addEventListener('push', (e) => {
-    let data = e.data ? e.data.json() : {};
+self.addEventListener("push", (e) => {
+  let data = e.data ? e.data.json() : {};
 
-    let options = {
-        body: data.body || "This notification was generated from a push :) !",
-        vibrate: [100, 50, 100],
-        data: {
-            dateOfArrival: Date.now(),
-            primaryKey: '2',
-        },
-        actions: [
-            {
-                action: 'explore',
-                title: 'Explore this new world!',
-            },
-            {
-                action: 'close',
-                title: 'Close',
-            }
-        ]
-    };
-    e.waitUntil(
-        self.registration.showNotification(data.title || 'Namaste universe', options)
-    );
+  let options = {
+    body: data.body || "This notification was generated from a push :) !",
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: "2",
+    },
+    actions: [
+      {
+        action: "explore",
+        title: "Explore this new world!",
+      },
+      {
+        action: "close",
+        title: "Close",
+      },
+    ],
+  };
+  e.waitUntil(
+    self.registration.showNotification(
+      data.title || "Namaste universe",
+      options
+    )
+  );
 });
